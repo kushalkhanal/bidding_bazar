7bc3c9a4affd75f1e4ed9f9f4da0f4ed982fc36e
kushalkhanal <kushalkhanal181@gmail.com>
2025-05-18 21:18:52 +0545

    splash screen added

---
+++ lib/view/splash_view.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:bidding_bazar/view/login_view.dart';
+
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginView()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Lottie.asset('assets/animations/splash.json')),
+    );
+  }
+}
