958b6c388d2608c6fe6aae737c629a84da6ff274
Kushal <kushalkhanal181@gmail.com>
2025-08-03 15:53:43 +0545

    Create constants.dart

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  // IMPORTANT: For Android emulator, use 10.0.2.2 instead of localhost
+  // For iOS simulator, localhost works fine.
+  static const String baseUrl = "http://10.0.2.2:5050/api";
+}
