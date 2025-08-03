68cab3acfa5d74da62d9c3efdb5d217084a959b5
Kushal <kushalkhanal181@gmail.com>
2025-08-03 15:51:02 +0545

    segregation of random files

--- lib/features/auth/data/data_source/user_data_source.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:bidding_bazar/features/auth/domain/entity/user_entity.dart';
-
-abstract class IUserDataSource {
-  Future<void> registerUser(UserEntity entity);
-  Future<String> loginUser(String email, String password);
-}
